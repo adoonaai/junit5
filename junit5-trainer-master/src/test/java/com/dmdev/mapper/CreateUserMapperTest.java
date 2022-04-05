8834583f9b460c93066f8ae1dfecf4a355391714
andrey.korytin <themarkling@gmail.com>
2022-04-06 00:14:25 +0300

    Junit 5

---
+++ junit5-trainer-master/src/test/java/com/dmdev/mapper/CreateUserMapperTest.java
@@ -0,0 +1,27 @@
+package com.dmdev.mapper;
+
+import com.dmdev.entity.User;
+import com.dmdev.generator.UserGenerator;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CreateUserMapperTest {
+
+    private final CreateUserMapper createUserMapper = CreateUserMapper.getInstance();
+
+    @Test
+    void mapCreateUserDtoToUser() {
+        var actualResult = createUserMapper.map(UserGenerator.getCreateUserDto());
+        var expectedResult = UserGenerator.getUser();
+
+        assertEquals(actualResult.getName(),expectedResult.getName());
+        assertEquals(actualResult.getBirthday(),expectedResult.getBirthday());
+        assertEquals(actualResult.getEmail(),expectedResult.getEmail());
+        assertEquals(actualResult.getPassword(),expectedResult.getPassword());
+        assertEquals(actualResult.getRole(),expectedResult.getRole());
+        assertEquals(actualResult.getGender(),expectedResult.getGender());
+    }
+}
